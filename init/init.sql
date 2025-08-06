4b5aeb4425ee5471de99655ee88582d6732bf594
Nutan <nutanpasham@gmail.com>
2025-08-06 12:39:20 +0530

    Fixed sidecar Dockerfile, added requirements, updated init.sql and deployment

--- init/init.sql
+++ init/init.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS changes (
     id SERIAL PRIMARY KEY,
     author TEXT NOT NULL,
-    timestamp TIMESTAMPTZ NOT NULL DEFAULT now()
+    timestamp TIMESTAMPTZ NOT NULL DEFAULT now(),
+    content TEXT NOT NULL
 );
+
